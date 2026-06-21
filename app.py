91d239073eb443a97d029c07b8756262eabcd6d6
hajraa001 <hajraaghayur@gmail.com>
2026-06-22 01:42:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Financial Analyst App")
+
+uploaded_file = st.file_uploader(
+    "Upload Financial Statement",
+    type=["xlsx"]
+)
+
+if uploaded_file:
+
+    df = pd.read_excel(uploaded_file)
+
+    st.subheader("Uploaded Data")
+    st.dataframe(df)
+
+    st.success("File uploaded successfully!")
+  
