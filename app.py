bdaba3b64f84c65e62319d67d470deae2bd9203e
hajraa001 <hajraaghayur@gmail.com>
2026-06-22 01:59:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,34 @@ import pandas as pd
 
 st.title("Financial Analyst App")
 
-uploaded_file = st.file_uploader(
-    "Upload Financial Statement",
+balance_file = st.file_uploader(
+    "Upload Balance Sheet",
     type=["xlsx"]
 )
 
-if uploaded_file:
+income_file = st.file_uploader(
+    "Upload Income Statement",
+    type=["xlsx"]
+)
 
-    df = pd.read_excel(uploaded_file)
+cashflow_file = st.file_uploader(
+    "Upload Cash Flow Statement",
+    type=["xlsx"]
+)
 
-    st.subheader("Uploaded Data")
-    st.dataframe(df)
+if balance_file and income_file and cashflow_file:
 
-    st.success("File uploaded successfully!")
-  
+    st.success("All 3 statements uploaded!")
+
+    balance_df = pd.read_excel(balance_file, skiprows=3)
+    income_df = pd.read_excel(income_file, skiprows=3)
+    cashflow_df = pd.read_excel(cashflow_file, skiprows=3)
+
+    st.subheader("Balance Sheet")
+    st.dataframe(balance_df.head(20))
+
+    st.subheader("Income Statement")
+    st.dataframe(income_df.head(20))
+
+    st.subheader("Cash Flow Statement")
+    st.dataframe(cashflow_df.head(20))
